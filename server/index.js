a80d8f40cd273a81ef527af445fa0d434b107877
furkan-cloud <firstinfirstout2020@gmail.com>
2021-02-28 01:24:15 +0300

    server created, mongodb connected

--- server/index.js
+++ server/index.js
@@ -2,5 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
 
-const express = require("express");
+// const express = require("express");
+
+const app = express();
+
+dotenv.config();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// require("dotenv").config();
+
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.error(error.message));
+
+mongoose.set("useFindAndModify", false);
