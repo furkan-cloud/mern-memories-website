28773e080105388d4c89a11dbbb8f48e469f058a
furkan-cloud <firstinfirstout2020@gmail.com>
2021-03-21 01:21:54 +0300

    store and reducers added

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  reducers,
+  compose(reducers, compose(applyMiddleware(thunk)))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
