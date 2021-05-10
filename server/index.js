1e447e86a88b9ca1e7598034b2adfeb27b2fb7e8
furkan-cloud <firstinfirstout2020@gmail.com>
2021-05-10 04:49:14 +0300

    url del

--- server/index.js
+++ server/index.js
@@ -9,22 +9,22 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-dotenv.config();
-
-app.use("/posts", postRoutes);
+// dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// require("dotenv").config();
+// should be under cors
+app.use("/posts", postRoutes);
 
+// require("dotenv").config();
 
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
